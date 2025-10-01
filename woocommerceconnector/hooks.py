37573160e0fa6234f322f44d3efedbb76cc9d9db
Ali <ali@example.com>
2025-10-01 22:40:44 +0300

    v32

--- woocommerceconnector/hooks.py
+++ woocommerceconnector/hooks.py
@@ -1,3 +1,13 @@
+app_name = "woocommerce_connector"
+app_title = "WooCommerce Connector"
+app_publisher = "Your Name"
+app_description = "Sync products and orders between ERPNext and WooCommerce"
+app_icon = "octicon octicon-globe"
+app_color = "blue"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# ربط الأحداث
 doc_events = {
     "Item": {
         "after_insert": "woocommerce_connector.sync_products.sync_product",
@@ -5,6 +15,7 @@ doc_events = {
     }
 }
 
+# مهام مجدولة
 scheduler_events = {
     "all": [
         "woocommerce_connector.sync_orders.sync_orders"
